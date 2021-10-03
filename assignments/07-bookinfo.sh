cd899660b83342dc6465a819a9af2c23d143d9aa
Poohcid Rugrawi <61070166@it.kmitl.ac.th>
2021-10-03 05:43:07 +0000

    update reviews dockerfile

--- assignments/07-bookinfo.sh
+++ assignments/07-bookinfo.sh
@@ -5,6 +5,6 @@ docker build -t details bookinfo/details/.
 docker build -t productpage bookinfo/productpage/.
 docker run -d --name mongodb -p 27017:27017 -v $(pwd)/bookinfo/ratings/databases:/docker-entrypoint-initdb.d bitnami/mongodb:5.0.2-debian-10-r2
 docker run -d --name ratings -p 8080:8080 --link mongodb:mongodb -e SERVICE_VERSION=v2 -e 'MONGO_DB_URL=mongodb://mongodb:27017/ratings' ratings
-docker run -d --name reviews --link ratings:ratings -p 8082:8082 -e "RATINGS_SERVICE=http://ratings:8080" -e "ENABLE_RATINGS=true" reviews
+docker run -d --name reviews -p 8082:9080 --link ratings:ratings -e "RATINGS_SERVICE=http://ratings:8080" -e "ENABLE_RATINGS=true" reviews
 docker run -d --name details -p 8081:8081 -e "ENABLE_EXTERNAL_BOOK_SERVICE=true" details
-docker run -d --name productpage --link details:details --link reviews:reviews -p 8083:8083 --link ratings:ratings -e "DETAILS_HOSTNAME=http://details:8081" -e "REVIEWS_HOSTNAME=http://reviews:8082" -e "RATINGS_HOSTNAME=http://ratings:8080" productpage
+docker run -d --name productpage -p 8083:8083 --link details:details --link reviews:reviews --link ratings:ratings -e "DETAILS_HOSTNAME=http://details:8081" -e "REVIEWS_HOSTNAME=http://reviews:9080" -e "RATINGS_HOSTNAME=http://ratings:8080" productpage
